bf25e8bd488fb3097df6fb2876a82120afd00e5f
Hyo-Seong <dlgytjd0907@naver.com>
2019-04-16 14:54:49 +0900

    블루투스 끄고 켜기

--- app/src/main/java/dgsw/hs/kr/premission/MainActivity.java
+++ app/src/main/java/dgsw/hs/kr/premission/MainActivity.java
@@ -1,6 +1,11 @@
 package dgsw.hs.kr.premission;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -8,24 +13,67 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQ_SEND_SMS = 1;
+    ToggleButton toggleButton;
+    final BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toggleButton = findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener((v, b) -> {
+            if(b){
+                startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),1);
+            } else {
+                bAdapter.disable();
+            }
+        });
 
         WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://naver.com");
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Receiver", "onReceive");
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
+            switch(state){
+                case BluetoothAdapter.STATE_ON:
+                    Log.d("Receiver", "Bluetooth ON");
+                    toggleButton.setChecked(true);
+                    break;
+                case BluetoothAdapter.STATE_OFF:
+                    Log.d("Receiver", "Bluetooth OFF");
+                    toggleButton.setChecked(false);
+                    break;
+            }
+        }
+    };
 
     public void onSendSMS(View v){
         if(Build.VERSION.SDK_INT >= 23){
